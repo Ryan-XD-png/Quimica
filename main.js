947faad092476bd12dc667b17fbd6d945cd5128d
Ryan <ryanestudoxd@gmail.com>
2025-09-09 02:42:15 -0300

    criação do menu lateral para a versão mobile

--- main.js
+++ main.js
@@ -26,7 +26,75 @@ botaoMais.addEventListener('click',()=>{
     return posi;
 })
 
-function fixedMenu(){
+
+let menu = document.getElementById('menuLateral');
+let header = document.getElementById('headerMenu');
+let botaoMenu = document.querySelector('.botaoMenu');  
+let aberto = false;
+let links = document.querySelectorAll('.aLateral');
+let ulH = document.getElementById('lateralMenu');
+let sair = document.getElementById('sair');
+let icone = document.getElementById('icone');
+let fundo = document.getElementById('fundo');
+const textMenu = ["Introdução","Tecnologias","Automação"];
+
+
+if(window.innerWidth <= 480){
+    
+    header.style.display = 'none';
+    botaoMenu.addEventListener('click',()=>{
+        
+        links.forEach((link, index) => {
+        link.textContent = textMenu[index];
+        link.style.color = 'white';
+        link.style.fontSize = '25px';
+        link.style.textDecoration = 'none';
+        link.style.fontWeight = 'bold';
+        link.style.listStyle = 'none';
+        link.style.margin = '0';
+        link.style.padding = '0';
+        link.style.cursor = 'pointer';
+        },);
+        fundo.style.position = 'fixed';
+        fundo.style.display = 'block';
+        fundo.style.width = '100vw';
+        fundo.style.height = '100vh';   
+        fundo.style.top = '0';
+        fundo.style.left = '0';
+        fundo.style.backgroundColor = 'rgba(0, 0, 0, 0.41)';
+        fundo.style.zIndex = '50';
+        sair.style.backgroundImage = "url('imgs/sair.svg')";
+        sair.style.width = '24px';
+        sair.style.height = '24px';
+        sair.style.position = 'absolute';
+        sair.style.top = '20px';
+        sair.style.left = '10px';
+        sair.style.cursor = 'pointer';
+        ulH.style.display = 'flex';
+        ulH.style.flexDirection = 'column';
+        ulH.style.alignItems = 'center';
+        ulH.style.height = '400px' ;
+        ulH.style.gap = '20px';
+        menu.style.display = 'flex';
+        menu.style.flexDirection = 'column';
+        menu.style.alignItems = 'center';
+        menu.style.paddingTop = '50px';
+        menu.style.gap = '20px';    
+        menu.style.position = 'absolute';
+        menu.style.top = '1rem';
+        menu.style.right = '0';
+        menu.style.borderRadius = '10px 0 0 10px';
+        menu.style.backgroundColor = '#004225';
+        menu.style.width = '60vw';
+        menu.style.height = '75vh';
+        menu.style.zIndex = '100';
+    });
 
 }
-document.addEventListener('click', fixedMenu);
+sair.addEventListener('click',()=>{
+    menu.style.display = 'none';
+    header.style.display = 'none';
+    botaoMenu.style.display = 'block';
+    aberto = false;
+    fundo.style.display = 'none';
+});
